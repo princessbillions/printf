3af83a731bb45f4b7ae71a5e53c8410668f38047
princess cherukei Ogunka <princesscherukei@gmail.com>
2022-04-20 13:59:24 +0100

    team task

--- print_add.c
+++ print_add.c
@@ -1,52 +1,39 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * print_add - prints the address of an input variable
- * @arguments: input address.
- * @buf: buffer pointer.
- * @ibuf: index for buffer pointer
- *
- * Return: number of chars printed.
- */
-int print_add(va_list arguments, char *buf, unsigned int ibuf)
-{
-	void *add;
-	long int int_input;
-	int i, count, first_digit, isnegative;
-	char *hexadecimal, *binary;
-	char nill[] = "(nil)";
 
-	add = (va_arg(arguments, void *));
-	if (add == NULL)
-	{
-		for (i = 0; nill[i]; i++)
-			ibuf = handl_buf(buf, nill[i], ibuf);
-		return (5);
-	}
-	int_input = (intptr_t)add;
-	isnegative = 0;
-	if (int_input < 0)
-	{
-		int_input = (int_input * -1) - 1;
-		isnegative = 1;
-	}
-	binary = malloc(sizeof(char) * (64 + 1));
-	binary = fill_binary_array(binary, int_input, isnegative, 64);
-	hexadecimal = malloc(sizeof(char) * (16 + 1));
-	hexadecimal = fill_hex_array(binary, hexadecimal, 0, 16);
-	ibuf = handl_buf(buf, '0', ibuf);
-	ibuf = handl_buf(buf, 'x', ibuf);
-	for (first_digit = i = count = 0; hexadecimal[i]; i++)
-	{
-		if (hexadecimal[i] != '0' && first_digit == 0)
-			first_digit = 1;
-		if (first_digit)
-		{
-			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
-			count++;
-		}
-	}
-	free(binary);
-	free(hexadecimal);
-	return (count + 2);
+/**
+ * get_print - selects the right printing function
+ * depending on the conversion specifier passed to _printf
+ * @s: character that holds the conversion specifier
+ * Description: the function loops through the structs array
+ * func_arr[] to find a match between the specifier passed to _printf
+ * and the first element of the struct, and then the approriate
+ * printing function
+ * Return: a pointer to the matching printing function
+ */
+int (*get_print(char s))(va_list, flags_t *)
+{
+	ph func_arr[] = {
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13},
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent}
+		};
+	int flags = 14;
+
+	register int i;
+
+	for (i = 0; i < flags; i++)
+		if (func_arr[i].c == s)
+			return (func_arr[i].f);
+	return (NULL);
 }
