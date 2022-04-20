0081d6ccfe0af491604922b38ddf50e70af1ab63
James Efienemokwu <jefiene77@gmail.com>
2022-04-20 10:43:29 +0100

    second comit

--- main.h
+++ main.h
@@ -6,17 +6,17 @@
 #include <stdlib.h>
 
 /**
- * struct print - struct for printer functions
- * @type_arg: identifier
- * @f: pointer to a printer functions
- *
- * Description: struct that stores pointers to a
- * printer functions.
- */
+* struct print - struct for printer functions
+* @type_arg: identifier
+* @f: pointer to a printer functions
+*
+* Description: struct that stores pointers to a
+* printer functions.
+*/
 typedef struct print
 {
-	char *type_arg;
-	int (*f)(va_list, char *, unsigned int);
+char *type_arg;
+int (*f)(va_list, char *, unsigned int);
 } print_t;
 
 int _printf(const char *format, ...);
