1a079c75e27a9cb5427d33268ba979a447415714
princess cherukei Ogunka <princesscherukei@gmail.com>
2022-04-20 14:44:44 +0100

    team task

--- print_add.c
+++ print_add.c
@@ -1,39 +1,25 @@
 #include "main.h"
 
 /**
- * get_print - selects the right printing function
- * depending on the conversion specifier passed to _printf
- * @s: character that holds the conversion specifier
- * Description: the function loops through the structs array
- * func_arr[] to find a match between the specifier passed to _printf
- * and the first element of the struct, and then the approriate
- * printing function
- * Return: a pointer to the matching printing function
+ * print_address - prints address of input in hexa format
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: number of char printed
  */
-int (*get_print(char s))(va_list, flags_t *)
+int print_address(va_list l, flags_t *f)
 {
-	ph func_arr[] = {
-		{'i', print_int},
-		{'s', print_string},
-		{'c', print_char},
-		{'d', print_int},
-		{'u', print_unsigned},
-		{'x', print_hex},
-		{'X', print_hex_big},
-		{'b', print_binary},
-		{'o', print_octal},
-		{'R', print_rot13},
-		{'r', print_rev},
-		{'S', print_bigS},
-		{'p', print_address},
-		{'%', print_percent}
-		};
-	int flags = 14;
+    char *str;
+    unsigned long int p = va_arg(l, unsigned long int);
 
-	register int i;
+    register int count = 0;
 
-	for (i = 0; i < flags; i++)
-		if (func_arr[i].c == s)
-			return (func_arr[i].f);
-	return (NULL);
+    (void)f;
+
+    if (!p)
+        return (_puts("(nil)"));
+    str = convert(p, 16, 1);
+    count += _puts("0x");
+    count += _puts(str);
+    return (count);
 }
